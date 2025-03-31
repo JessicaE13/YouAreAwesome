5fa839f5822872309a84f7b50b596d6a8df88df9
Jessica Estes <jessica.m.estes@gmail.com>
2025-03-30 22:45:27 -0400

    After While and Repeat While loops

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -12,11 +12,13 @@ struct ContentView: View {
     @State private var imageName = ""
     @State private var imageNumber = 0
     @State private var messageNumber = 0
+    @State private var lastImageNumber = -1
+    @State private var lastMessageNumber = -1
     
     var body: some View {
         
         VStack {
-     
+            
             Text(message)
                 .font(.largeTitle)
                 .fontWeight(.heavy)
@@ -45,9 +47,22 @@ struct ContentView: View {
                                 "Yass Queen!",
                                 "You Are Beautiful Like A Butterfly"]
                 
-                message = messages[Int.random(in: 0...messages.count-1)]
-
-                imageName = "image\(Int.random(in: 0...9))"
+                repeat{
+                    messageNumber = Int.random(in: 0...messages.count-1)
+                } while messageNumber == lastMessageNumber
+                message = messages[messageNumber]
+                lastMessageNumber = messageNumber
+                
+                repeat{
+                    imageNumber = Int.random(in: 0...9)
+                } while imageNumber == lastImageNumber
+                imageName = "image\(imageNumber)"
+                lastImageNumber = imageNumber
+                
+                
+                
+                
+                
             }
             .buttonStyle(.borderedProminent)
             .font(.title2)
